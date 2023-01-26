a0a3f2d952cabcc4814d5987fe72d57e224e5b61
Jb <jb@levillageducode.fr>
2023-01-26 10:28:37 +0100

    Configure CRUD for basic fields and entity name

--- src/Controller/Admin/AnswerCrudController.php
+++ src/Controller/Admin/AnswerCrudController.php
@@ -4,12 +4,19 @@ namespace App\Controller\Admin;
 
 use App\Entity\Answer;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 
 class AnswerCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return Answer::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud {
+        return $crud
+        ->setEntityLabelInSingular('Réponse')
+        ->setEntityLabelInPlural('Réponses');
     }
 
     /*
--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -5,6 +5,10 @@ namespace App\Controller\Admin;
 use App\Entity\Category;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+
+
 class CategoryCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
@@ -12,14 +16,11 @@ class CategoryCrudController extends AbstractCrudController
         return Category::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
             TextField::new('title'),
             TextEditorField::new('description'),
         ];
     }
-    */
 }
