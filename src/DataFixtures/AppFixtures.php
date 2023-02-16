86c9f037acf2fa458262cca681a8477a858b47fc
Jb <jb@levillageducode.fr>
2023-02-16 10:16:11 +0100

    Generate Fixtures with one relaiton between Category and Quiz

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,25 +6,53 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 use App\Entity\Category;
+use App\Entity\Quiz;
 
 class AppFixtures extends Fixture
 {
+    // public function randomHex() {
+    //     $chars = 'ABCDEF0123456789';
+    //     $color = '#';
+    //     for ( $i = 0; $i < 6; $i++ ) {
+    //       $color .= $chars[rand(0, strlen($chars) - 1)];
+    //     }
+    //     return $color;
+    // }
+
+
     public function load(ObjectManager $manager): void
     {
-        // 1. Générer une nouvelle instance de l'entité
+
+    //     for($i=1; $i<=15;$i++) {
+    //         // 1. Générer une nouvelle instance de l'entité
+    //         $category = new Category();
+
+    //         // 1bis. Préciser des valeurs pour les propriétés de votre
+    //         // futur enregistrement
+    //         $category->setTitle("Catégorie numéro ".$i);
+    //         $category->setDescription("Description de la catégorie numéro ".$i);
+    //         $category->setColor($this->randomHex());
+
+    //         // 2. Prendre en compte votre futur enregistrement
+    //         // Pour un potentiel ajout dans la BDD (PAS DE SQL FAIT)
+
+    //         $manager->persist($category);
+    //         }
+
+    //         // 3. Exécution d'une requête SQL (INSERT INTO)
+    //         $manager->flush();
+
         $category = new Category();
-
-        // 1bis. Préciser des valeurs pour les propriétés de votre
-        // futur enregistrement
-        $category->setTitle("Dinosaures");
-        $category->setDescription("Tout savoir sur ces fabuleux reptiles");
-
-
-        // 2. Prendre en compte votre futur enregistrement
-        // Pour un potentiel ajout dans la BDD (PAS DE SQL FAIT)
+        $category->setTitle("Titre de ma catégorie");
         $manager->persist($category);
+        $this->addReference('maCategoriequitue', $category);
 
-        // 3. Exécution d'une requête SQL (INSERT INTO)
+        $quiz = new Quiz();
+        $newCategory = $this->getReference('maCategoriequitue');
+        $quiz->setCategory($newCategory);
+        $quiz->setTitle("Mon quiz qui tache");
+        $manager->persist($quiz);
         $manager->flush();
     }
+
 }
