0c1f34519afda0aac2d0c9e7527ece0826e9ca06
Jb <jb@levillageducode.fr>
2023-02-16 12:19:31 +0100

    Load 1 admin in fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,19 +6,67 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 use Faker;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 use App\Entity\Category;
 use App\Entity\Quiz;
+use App\Entity\User;
 
 class AppFixtures extends Fixture
 {
-    public function randomHex() {
-        $chars = 'ABCDEF0123456789';
-        $color = '#';
-        for ( $i = 0; $i < 6; $i++ ) {
-          $color .= $chars[rand(0, strlen($chars) - 1)];
+    private $userPasswordHasherInterface;
+
+    public function __construct (UserPasswordHasherInterface $userPasswordHasherInterface)
+    {
+    $this->userPasswordHasherInterface = $userPasswordHasherInterface;
+    }
+
+    public function loadAdmin(ObjectManager $manager) {
+        $admin = new User();
+        $admin->setEmail('jb@jb.fr');
+        $admin->setRoles(['ROLE_ADMIN']);
+
+        $admin->setPassword(
+          $this->userPasswordHasherInterface->hashPassword(
+            $admin, 'root1234'
+          )
+        );
+
+        $manager->persist($admin);
+        $manager->flush();
+    }
+
+    public function loadCategories(ObjectManager $manager, $faker) {
+        for($i=1; $i<=15;$i++) {
+            // 1. Générer une nouvelle instance de l'entité
+            $category = new Category();
+
+            // 1bis. Préciser des valeurs pour les propriétés de votre
+            // futur enregistrement
+            $category->setTitle($faker->sentence());
+            $category->setDescription($faker->paragraph());
+            $category->setColor($faker->hexColor());
+
+            // 2. Prendre en compte votre futur enregistrement
+            // Pour un potentiel ajout dans la BDD (PAS DE SQL FAIT)
+
+            $manager->persist($category);
+            $this->addReference('category_'.$i, $category);
         }
-        return $color;
+        $manager->flush();
+    }
+
+    public function loadQuizzes(ObjectManager $manager, $faker) {
+
+        for($i=1; $i<=30;$i++) {
+            $quiz = new Quiz();
+            $currentCategory = $this->getReference('category_'.mt_rand(1,15));
+            $quiz->setCategory($currentCategory);
+            $quiz->setTitle($faker->sentence());
+            $quiz->setImg($faker->image('public/uploads/img/', 640, 480, '', false));
+            $manager->persist($quiz);
+        }
+        $manager->flush();
     }
 
 
@@ -27,36 +75,9 @@ class AppFixtures extends Fixture
 
         $faker = Faker\Factory::create("fr_FR");
 
-        for($i=1; $i<=15;$i++) {
-            // 1. Générer une nouvelle instance de l'entité
-            $category = new Category();
-
-            // 1bis. Préciser des valeurs pour les propriétés de votre
-            // futur enregistrement
-            $category->setTitle($faker->sentence());
-            $category->setDescription("Description de la catégorie numéro ".$i);
-            $category->setColor($this->randomHex());
-
-            // 2. Prendre en compte votre futur enregistrement
-            // Pour un potentiel ajout dans la BDD (PAS DE SQL FAIT)
-
-            $manager->persist($category);
-            $this->addReference('category_'.$i, $category);
-            }
-
-
-        for($i=1; $i<=30;$i++) {
-            $quiz = new Quiz();
-            $currentCategory = $this->getReference('category_'.mt_rand(1,15));
-            $quiz->setCategory($currentCategory);
-            $quiz->setTitle("Quiz numéro ".$i);
-            $manager->persist($quiz);
-        }
-
-
-        // 3. Exécution d'une requête SQL (INSERT INTO)
-        $manager->flush();
-
+        $this->loadCategories($manager,$faker);
+        $this->loadQuizzes($manager,$faker);
+        $this->loadAdmin($manager);
     }
 
 }
